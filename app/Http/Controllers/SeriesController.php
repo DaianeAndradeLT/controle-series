6b1b92d12ca42dfbc65938a05c76c7460f524b89
Daiane Andrade <daianeandradelt@hotmail.com>
2024-04-23 16:24:58 -0300

    Initial commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(request $request)
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+
+        ];
+
+        return view('listar-series')->with('series', $series);
+
+    }
+}
